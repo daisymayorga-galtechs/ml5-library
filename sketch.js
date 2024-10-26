a5f39217c92f484aea172597272a048150a75f86
daisymayorga-galtechs <68982348+daisymayorga-galtechs@users.noreply.github.com>
2024-10-26 02:03:56 -0700

    updated

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
+//creating a variable for our image classifier 
 let mobilenet;
 
 let bird;
 
+
+//this is the function for callback modelReady in the image classification object stored in the mobilenet variable
 function modelReady(){
     console.log('Model is ready!!');
 
@@ -32,14 +35,19 @@ function imageReady()   {
     image(bird, 0, 0, width, height);
 }
 
+//p5js library supports a setup function, which executes when the web page loads
 function setup() {
 
+    // createCanvas makes a sketch canvas using the p5js library 
     createCanvas(640, 480);
     bird = createImg('assets/bird.jpg', imageReady);
     bird.hide();
 
+    //background color of the canvas is black
     background(0);
     
+    //this is a function that generates an image classification object that will be stored in the variable 'mobilenet'
+    //The first argument is a string with the name of the model 'MobileNet' and second is  a callback (the ML5 library supports callbacks and promises)
     mobilenet = ml5.imageClassifier('MobileNet', modelReady);
 
 }
